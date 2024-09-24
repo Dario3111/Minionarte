ecf8ed3efb69e03a5005d01ca43bacbe0306bbfb
merkandez <131488754+merkandez@users.noreply.github.com>
2024-09-24 15:35:23 +0200

    actualización connection_db

--- database/connection_db.js
+++ database/connection_db.js
@@ -1,8 +1,8 @@
 // Hola Mundo
 import { Sequelize } from "sequelize";
 
-const connection_db = new Sequelize("book_app", "root", "Pikachu2609>v", {
-  host: "localhost",
+const connection_db = new Sequelize("book_app", process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   define: {
     timestamps: false,
