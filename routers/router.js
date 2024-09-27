c9699f2efe3756baed302346fba7e31426040bc7
merkandez <131488754+merkandez@users.noreply.github.com>
2024-09-27 15:36:35 +0200

    controllers y routes montados y funcionando

--- routers/router.js
+++ routers/router.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { getAllMemes} from "../controllers/minionController.js";
+import { getAllMemes, createMeme, getMemeById, deleteMeme, updateMeme } from "../controllers/minionController.js";
 
 const router = express.Router();
 
 
 router.get("/memes", getAllMemes); // Obtener todos los memes
-// router.post("/memes", crearMeme); // Crear un meme
-// router.get("/memes/:id", obtenerMemePorId); // Obtener un meme por ID
-// router.put("/memes/:id", actualizarMeme); // Actualizar un meme
-// router.delete("/memes/:id", eliminarMeme); // Eliminar un meme
+router.post("/memes", createMeme); // Crear un meme
+router.get("/memes/:id", getMemeById); // Obtener un meme por ID
+router.put("/memes/:id", updateMeme); // Actualizar un meme
+router.delete("/memes/:id", deleteMeme); // Eliminar un meme
 
 export default router;
